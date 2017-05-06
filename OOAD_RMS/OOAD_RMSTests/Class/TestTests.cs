db23949d74f16f9ba59f063bdf06ac74b026d469
Joker <jaker743835@gmail.com>
2017-05-06 17:40:28 +0800

    ut

---
+++ OOAD_RMS/OOAD_RMSTests/Class/TestTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OOAD_RMS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOAD_RMS.Tests
+{
+    [TestClass()]
+    public class TestTests
+    {
+        [TestMethod()]
+        public void AddRequirementTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
